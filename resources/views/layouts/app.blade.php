c6650dda7fae4a805986c16ec67cb508fffee62a
Stefan <stefan.panic@outlook.com>
2016-03-06 18:28:09 +0100

    finished home section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Makers Vault</title>
     <link rel="stylesheet" type="text/css" href="/assets/css/app.css">
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600,700,900' rel='stylesheet' type='text/css'>
   </head>
   <body>
     @yield('content')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="hero">
+<section class="home">
   <div class="row">
     <div class="small-12 columns">
-      <h2>Makersvault</h2>
+      <h2 class="text-center">Product Name</h2>
+    </div>
+    <div class="small-12 large-8 columns">
+      <img src="http://placehold.it/800x600" alt="" />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <div class="">
+        <a href="#" class="button expanded">Purchase for 30$</a>
+      </div>
+    </div>
+    <div class="small-12 large-4 columns">
+      <div class="callout">
+        <p>Maker: Stefan</p>
+      </div>
     </div>
   </div>
 </section>
